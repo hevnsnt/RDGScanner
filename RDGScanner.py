45a24f9a60cba43b0681915f3bffc47e63a4dadd
Bill Swearingen <hevnsnt@gmail.com>
2020-01-27 08:07:20 -0600

    cleaning some stuff up

--- RDGScanner.py
+++ RDGScanner.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 #
-# Single check to see if the server is still vulnerable to CVE-2020-0609
-# 
+# Single check to see if the server is still vulnerable to CVE-2019-19781
+# Written by: Dave Kennedy, @HackingDave
+# Company: TrustedSec
 #
 from OpenSSL import SSL
 from OpenSSL._util import (lib as _lib)
@@ -9,19 +10,24 @@ import sys
 import socket
 import struct
 import select
-from netaddr import IPNetwork
 import argparse
+from netaddr import IPNetwork
 import threading
 import time
 import subprocess
 import re
 
+#Set Script variables
 # PyOpenSSL doesn't expose the DTLS method to python, so we have to patch it
 DTLSv1_METHOD = 7
 SSL.Context._methods[DTLSv1_METHOD] = getattr(_lib, "DTLSv1_client_method")
-
 vulnerable = True
 connected = False
+vulnServers = []
+counter = 0
+CVEvuln = "CVE-2020-0610"
+
+
 
 def asn_to_ip(asn):
     # use ASN listings to enumerate whois information for scanning.
@@ -39,6 +45,61 @@ def asn_to_ip(asn):
     for cidr in asn_convert:
         cidr_list.append(cidr)
     return cidr_list
+
+def build_connect_packet(fragment_id, num_fragments, data):
+    packet_type = 5
+    packet_len = len(data) + 6
+    fragment_id = fragment_id
+    num_fragments = num_fragments
+    fragment_len = len(data)
+    data = data
+
+    packet = struct.pack('<HHHHH', packet_type, packet_len, fragment_id,
+                         num_fragments, fragment_len)
+    packet += data
+    return packet
+
+
+def certificate_callback(sock, cert, err_num, depth, ok):
+    global connected
+
+    server_name = cert.get_subject().commonName
+    print('Got certificate for server: %s' % server_name)
+
+    connected = True
+    return True
+
+
+def check_server(ip, port, timeout):
+    global vulnerable
+    global CVEvuln
+
+    print("Scanning: %s        " % ip, end="\r") # Cleaning up output a little
+    ctx = SSL.Context(DTLSv1_METHOD)
+    ctx.set_verify_depth(2)
+    ctx.set_verify(SSL.VERIFY_PEER, certificate_callback)
+
+    sock = SSL.Connection(ctx, socket.socket(socket.AF_INET, socket.SOCK_DGRAM))
+
+    sock.connect((ip, int(port)))
+    sock.send(build_connect_packet(0, 65, b"A"))
+
+    read_fds, _, _ = select.select([sock], [], [], timeout)
+    if read_fds:
+        data = sock.recv(1024)
+        if len(data) == 16:
+            error_code = struct.unpack('<L', data[12:])[0]
+            if error_code == 0x8000ffff:
+                vulnerable = False
+
+    if connected:
+        if vulnerable:
+            print("[\033[91m!\033[0m]%s is vulnerable to %s" % (target,CVEvuln))
+            vulnServers.append(target)
+        else:
+            print('Scan Completed: server is not vulnerable')
+    else:
+        print('ERROR: could not connect to server')
 
 
 def parse_target_args(target, port, verbose):
@@ -70,55 +131,7 @@ def parse_target_args(target, port, verbose):
     # if we are just using 1 IP address
     else:
         counter = counter + 1 
-        scan_server(target, port, timeout_secs)
-        #check_server(target, port,verbose)
-
-
-def build_connect_packet(fragment_id, num_fragments, data):
-    packet_type = 5
-    packet_len = len(data) + 6
-    fragment_id = fragment_id
-    num_fragments = num_fragments
-    fragment_len = len(data)
-    data = data
-
-    packet = struct.pack('<HHHHH', packet_type, packet_len, fragment_id,
-                         num_fragments, fragment_len)
-    packet += data
-    return packet
-
-
-def certificate_callback(sock, cert, err_num, depth, ok):
-    global connected
-
-    server_name = cert.get_subject().commonName
-    print('Got certificate for server: %s' % server_name)
-
-    connected = True
-    return True
-
-
-def scan_server(ip, port, timeout):
-    global vulnerable
-
-    print('Checking {}:{}'.format(ip, port))
-
-    ctx = SSL.Context(DTLSv1_METHOD)
-    ctx.set_verify_depth(2)
-    ctx.set_verify(SSL.VERIFY_PEER, certificate_callback)
-
-    sock = SSL.Connection(ctx, socket.socket(socket.AF_INET, socket.SOCK_DGRAM))
-
-    sock.connect((ip, int(port)))
-    sock.send(build_connect_packet(0, 65, b"A"))
-
-    read_fds, _, _ = select.select([sock], [], [], timeout)
-    if read_fds:
-        data = sock.recv(1024)
-        if len(data) == 16:
-            error_code = struct.unpack('<L', data[12:])[0]
-            if error_code == 0x8000ffff:
-                vulnerable = False
+        check_server(target, port,verbose)
 
 def banner():
     print("""
@@ -146,14 +159,23 @@ def banner():
     """)
 
 
+def dummy():
+    pass
+
 if __name__ == '__main__':
     # if server doesn't respond before timeout, we assume it's patched
     # setting the timeout too low can result in false
     timeout_secs = 3
 
-    #scan_server(sys.argv[1], sys.argv[2], timeout_secs)
-    vulnServers = []
-    counter = 0
+    if connected:
+        if vulnerable:
+            print('Scan Completed: server is vulnerable')
+        else:
+            print('Scan Completed: server is not vulnerable')
+    else:
+        print('ERROR: could not connect to server')
+
+
     thread = threading.Thread(target=dummy)
     thread.start()
 
@@ -184,24 +206,15 @@ if __name__ == '__main__':
         else:
             parse_target_args(args.target, args.targetport, verbose)
 
+
+        for server in vulnServers:
+            print(server)
+
     except KeyboardInterrupt:
         print("[!] interrupt received, stopping..")
         time.sleep(0.1)
-
-        if connected:
-            if vulnerable:
-                print('Scan Completed: server is vulnerable')
-            else:
-                print('Scan Completed: server is not vulnerable')
-        else:
-            print('ERROR: could not connect to server')
 
     finally:
         # do a report on vuln servers
         print("Finished testing %s servers: Found %s to be vulnerable. Below is a list system(s) identified:" % (counter, len(vulnServers)))
         print("-" * 45)
-        for server in vulnServers:
-            print(server)
-
-
-
